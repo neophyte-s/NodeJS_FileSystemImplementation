8652980d6813454499b9d97ef198a262a3b1c6de
Swarnim <63869807+neophyte-s@users.noreply.github.com>
2020-05-18 16:26:37 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports.help=function(){
+    console.log(`Help command has been called :
+                             To view use view command
+                             To untreefy use untreefy command
+                             To treefy use treefy command
+                             To monitor use monitor command`);
+}
